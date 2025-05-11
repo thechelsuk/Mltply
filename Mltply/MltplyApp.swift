e7bc89a52e948678da39951e52a6e3e84d9ad33f
mat-0 <Github@thechels.uk>
2025-05-11 15:33:34 +0100

    Initial Commit

---
+++ Mltply/MltplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MltplyApp.swift
+//  Mltply
+//
+//  Created by Mat Benfield on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MltplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
